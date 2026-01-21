180434eb5d3f678319490cf497219cd429f65654
meiyouname <14792102+meiyouname@user.noreply.gitee.com>
2026-01-21 17:50:18 +0800

    aaa

--- demo.cpp
+++ demo.cpp
@@ -1,42 +1,92 @@
-#include <stdio.h>  // 包含标准输入输出头文件
+#include <stdio.h>
+typedef char VertexType;
+typedef int EdgeType;
+#define MAXSIZE 100
+typedef struct 
+{
+    VertexType vertex[MAXSIZE];
+    EdgeType arc[MAXSIZE][MAXSIZE];
+    int vertex_num;
+    int edge_num;
+}Mat_Grph;
 
-// 定义一个计算两数之和的函数
-int add_numbers(int a, int b) {
-    return a + b;
+int visited[MAXSIZE];
+
+
+void create_graph(Mat_Grph* G){
+    G->vertex_num=9;
+    G->edge_num=15;
+    G->vertex[0]='A';
+    G->vertex[1]='B';
+    G->vertex[2]='C';
+    G->vertex[3]='D';
+    G->vertex[4]='E';
+    G->vertex[5]='F';
+    G->vertex[6]='G';
+    G->vertex[7]='H';
+    G->vertex[8]='I';
+
+    for (int i = 0; i < G->vertex_num; i++)
+    {
+        for (int j = 0; j < G->vertex_num; j++)
+        {
+            G->arc[i][j]=0;
+        }
+        
+    }
+    G->arc[0][1]=1;
+    G->arc[0][5]=1;
+    G->arc[1][2]=1;
+    G->arc[0][1]=1;
+    G->arc[1][6]=1;
+    G->arc[1][8]=1;
+
+    G->arc[2][3]=1;
+    G->arc[2][8]=1;
+
+    G->arc[3][4]=1;
+    G->arc[3][6]=1;
+    G->arc[3][7]=1;
+    G->arc[3][8]=1;
+
+    G->arc[4][5]=1;
+    G->arc[4][7]=1;
+
+    G->arc[5][6]=1;
+
+    G->arc[6][7]=1;
+    for (int i = 0; i < G->vertex_num; i++)
+    {
+        for (int j = 0; j < G->vertex_num; j++)
+        {
+            if(G->arc[i][j]==1) G->arc[j][i]=1;
+        }
+        
+    }
 }
 
-// 主函数，程序入口
-int main() {
-    // 变量声明和初始化
-    int num1 = 10;
-    int num2 = 20;
-    int sum = 0;
-    
-    // 调用函数
-    sum = add_numbers(num1, num2);
-    
-    // 打印结果
-    printf("数字 %d 和 %d 的和是: %d\n", num1, num2, sum);
-    
-    // 用户输入示例
-    int user_input;
-    printf("请输入一个整数: ");
-    scanf("%d", &user_input);
-    printf("你输入的是: %d\n", user_input);
-    
-    // 条件判断
-    if (user_input > 100) {
-        printf("这个数大于100\n");
-    } else {
-        printf("这个数小于等于100\n");
+void dfs(Mat_Grph G,int i){
+    visited[i]=1;
+    printf("%c\n",G.vertex[i]);
+    for (int j = 0; j < G.vertex_num; j++)
+    {
+        if (G.arc[i][j]==1&&visited[j]==0)
+        {
+            dfs(G,j);
+        }
+        
     }
-    
-    // 循环示例
-    printf("从1数到5:\n");
-    for (int i = 1; i <= 5; i++) {
-        printf("%d ", i);
+}
+
+
+
+
+int main(){
+    Mat_Grph G;
+    create_graph(&G);
+    for (int i = 0; i < G.vertex_num; i++)
+    {
+        visited[i]=0;
     }
-    printf("\n");
-    
-    return 0;  // 程序正常结束
+    dfs(G,0);
 }
